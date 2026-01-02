6bbb4c5088e1b5d4da1c0a075b133b2bdeee2b08
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-01-02 12:49:05 +0600

    added geekforgeeks 5 no question answer

---
+++ 2. Searching Algorithms/2. Binary Search/geeksforgeeks_binary-search_practice/geeksforgeeks_answe_05.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+void search(int arr[], int size, int k){
+    int low = 0;
+    int high = size - 1;
+
+    while(low <= high){
+        int mid = low + (high - low)/2;
+
+        if(arr[mid] == k){
+            printf("%d", mid);
+            return;
+        }
+
+        if(arr[mid] >= arr[low]){
+            if(k >= arr[low] && k < arr[high]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        else{
+            if(k > arr[mid] && k <= arr[high]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+    }
+
+    if(low > high){
+        printf("-1");
+    }
+}
+
+
+
+int main(){
+    int size;
+    scanf("%d", &size);
+
+    int arr[size];
+    for(int i = 0; i < size; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int k;
+    scanf("%d", &k);
+
+    search(arr, size, k);
+
+    return 0;
+}
